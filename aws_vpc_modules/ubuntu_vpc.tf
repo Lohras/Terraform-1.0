0a63d14553372bd40e3948834b7549ab13db9f06
lrurs <lohith.rajeursks@amadeus.com>
2022-09-24 11:40:39 +0000

    code vpc updated

--- aws_vpc_modules/ubuntu_vpc.tf
+++
@@ -1,9 +0,0 @@
-module "vpc" {
-  source = "./aws_vpc_modules/vpc"
- 
-  infra_env = var.infra_env
- 
-  # Note we are /17, not /16
-  # So we're only using half of the available
-  vpc_cidr  = "10.0.0.0/17"
-}
