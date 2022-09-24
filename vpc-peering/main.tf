e94ed7fe8e719bd1c1bb6fe3c6da7ac8ddfbb38c
lrurs <lohith.rajeursks@amadeus.com>
2022-09-24 21:26:27 +0000

    peering codes updated

--- vpc-peering/main.tf
+++ vpc-peering/main.tf
@@ -2,24 +2,37 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.33.0"
+      version = "4.34.0"
     }
   }
 }
 
-resource "aws_vpc_peering_connection" "vpc1" {
-  peer_owner_id = var.peer_owner_id
-  peer_vpc_id   = var.aws_vpc2
-  vpc_id        = var.aws_vpc1
-  peer_region   = "us-east-2"
+provider "aws" {
+  resource "aws_vpc" "vpc1" {
+    cidr_block = "192.168.0.0/16"
+  }
+
+  resource "aws_vpc" "vpc2" {
+    cidr_block = "10.0.0.0/16"
+  }
+
+  resource "aws_vpc_peering_connection" "vpc1" {
+    vpc_id      = aws_vpc.vpc1.id
+    peer_vpc_id = aws_vpc.vpc2.id
+    auto_accept = true
+  }  
+
+  resource "aws_vpc_peering_connection_options" "vpc1" {
+    vpc_peering_connection_id = aws_vpc_peering_connection.vpc1.id
+
+    accepter {
+      allow_remote_vpc_dns_resolution = true
+    }
+
+    requester {
+      allow_vpc_to_remote_classic_link = true
+      allow_classic_link_to_remote_vpc = true
+    }
+  }
 }
 
-resource "aws_vpc" "vpc2" {
-  provider   = aws.us-west-2
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_vpc" "vpc1" {
-  provider   = aws.us-east-2
-  cidr_block = "192.168.0.0/16"
-}
