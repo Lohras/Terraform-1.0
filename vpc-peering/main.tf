442e0c8973b749fa7c5408a2034468694cfcd275
lrurs <lohith.rajeursks@amadeus.com>
2022-09-25 08:38:03 +0000

    check

--- vpc-peering/main.tf
+++ vpc-peering/main.tf
@@ -1,19 +1,4 @@
-module "single_account_single_region" {
-  source = "terraform-aws-vpc-peering/examples/single-account-single-region"
-
-
-  providers = {
-    aws.this = aws
-    aws.peer = aws
-  }
-
-  this_vpc_id = var.this_vpc_id
-  peer_vpc_id = var.peer_vpc_id
-
-  auto_accept_peering = true
-
-  tags = {
-    Name        = "tf-single-account-single-region"
-    Environment = "Test"
-  }
+resource "aws_vpc_peering_connection" "my_peer" {
+  vpc_id = 
+  
 }
--- vpc-peering/variable.tf
+++ vpc-peering/variable.tf
@@ -9,13 +9,13 @@ variable "peer_vpc_id" {
 
 variable "aws_this_access_key" {
   type = string
-  default = AKIA3MGXYF3IQRO7QKXM
+  default =
   description = "AWS Access Key for requester account"
   
 }
 
 variable "aws_this_secret_key" {
   type = string
-  default = s0lUt2NimGn+GctnmYDeGP6IZrIK3xslA/gDmv9J
+  default = 
   description = "AWS Secret Key for requester account"
 }
