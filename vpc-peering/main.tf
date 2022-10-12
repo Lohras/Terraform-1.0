837bcc2134fc2a906cb0a5ea86759510f7451502
Lohith raje urs <101534920+Lohras@users.noreply.github.com>
2022-10-12 12:29:12 +0530

    Update main.tf

--- vpc-peering/main.tf
+++ vpc-peering/main.tf
@@ -24,10 +24,19 @@ resource "aws_vpc_peering_connection" "my_peer" {
   }
 }
 
+resource "aws_route_table" "requester" {
+  vpc_id = aws_vpc.vpc1.id
+  
+}
+
+resource "aws_route_table" "accepter" {
+  vpc_id = aws_vpc.default.id
+  
+}
 #creating routes from req-vpc2 to acc-default
 resource "aws_route" "requester" {
-  route_table_id = aws_route.requester.id
-  destination_cidr_block = "10.0.0.0/16"
+  route_table_id = aws_route_table.requester.id
+  destination_cidr_block = "172.31.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.my_peer.id
   
 }
@@ -35,8 +44,8 @@ resource "aws_route" "requester" {
 #Create routes from acc-default to req-vpc2
 
 resource "aws_route" "accepter" {
-  route_table_id = aws_route.accepter.id
-  destination_cidr_block = "172.31.0.0/16"
+  route_table_id = aws_route_table.accepter.id
+  destination_cidr_block = "10.0.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.my_peer.id
   
 }
