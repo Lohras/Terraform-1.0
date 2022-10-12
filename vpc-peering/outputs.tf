2b0958cf2f2df408c6d640563a4fabe37ef64eed
Lohith raje urs <101534920+Lohras@users.noreply.github.com>
2022-10-12 12:50:08 +0530

    Update outputs.tf

--- vpc-peering/outputs.tf
+++ vpc-peering/outputs.tf
@@ -1,3 +1,19 @@
 output "connection_id" {
   value = aws_vpc_peering_connection.my_peer.id
 }
+
+output "requester_id" {
+  value = aws_vpc.vpc1.id
+}
+
+output "acceptor_id" {
+  value = aws_vpc.default.id
+}
+
+output "requester_route_table" {
+  value =  aws_route_table.requester.id 
+}
+
+output "accepter_route_table" {
+  value = aws_route_table.accepter.id
+}
