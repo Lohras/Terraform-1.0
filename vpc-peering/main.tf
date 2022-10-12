5466ca18b24caf3490fbeca2187065f4eead3872
Lohith raje urs <101534920+Lohras@users.noreply.github.com>
2022-10-12 11:59:15 +0530

    Update main.tf

--- vpc-peering/main.tf
+++ vpc-peering/main.tf
@@ -1,22 +1,29 @@
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "default" {
+  cidr_block = "172.31.0.0/16"
+}
 resource "aws_vpc_peering_connection" "my_peer" {
-  vpc_id = "vpc-0de3699e0d9e7e186"
-  peer_vpc_id = "vpc-0e7724f33e892da9f"
+  vpc_id = aws_vpc.vpc1.id
+  peer_vpc_id = aws_vpc.default.id
 
   auto_accept = true
   
-  accepter {
-    allow_remote_vpc_dns_resolution = true
+  acceptor {
+    allow_remote_vpc_dns_solution = true
   }
 
-  requester {
-    allow_remote_vpc_dns_resolution = true
+  requestor {
+    allow_remote_vpc_dns_solution = true
   }
 }
 
 #creating routes from req-vpc2 to acc-default
-resource "aws_route" "requester" {
-  route_table_id = "rtb-0b0ea353ccafb2753"
-  destination_cidr_block = "172.31.0.0/16"
+resource "aws_route" "requestor" {
+  route_table_id = "rtb-00ce6fb93d8eb8f43"
+  destination_cidr_block = "10.0.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.my_peer.id
   
 }
@@ -24,8 +31,8 @@ resource "aws_route" "requester" {
 #Create routes from acc-default to req-vpc2
 
 resource "aws_route" "accepter" {
-  route_table_id = "rtb-0c24e2e75f6ea1395"
-  destination_cidr_block = "10.0.0.0/16"
+  route_table_id = "rtb-0208729dfa5c7fb47"
+  destination_cidr_block = "172.31.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.my_peer.id
   
 }
