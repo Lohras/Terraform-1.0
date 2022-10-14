1164e48d14c3118c55e92ab5ed43aad09f3a97f9
Lohith raje urs <101534920+Lohras@users.noreply.github.com>
2022-10-14 12:01:13 +0530

    Update data.tf

--- vpc-peering/data.tf
+++ vpc-peering/data.tf
@@ -1,10 +1,16 @@
-
-
 data "aws_vpc" "requester" {
   id = var.vpc_id
 }
 data "aws_vpc" "accepter" {
   id = var.peer_vpc_id
+}
+
+data "aws_subnet" "requester" {
+  id = var.req_subnet_id
+}
+
+data "aws_subnet" "acceptor" {
+  id = var.acc_subnet_id
 }
 
 data "aws_route_table" "requester" {
