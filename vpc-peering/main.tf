da419af59cd76dffa1509863427e538b0e94dbfe
Lohith raje urs <101534920+Lohras@users.noreply.github.com>
2022-10-14 12:06:46 +0530

    Update main.tf

--- vpc-peering/main.tf
+++ vpc-peering/main.tf
@@ -1,10 +1,10 @@
 resource "aws_vpc" "requester" {
-  req_cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
 }
 
 resource "aws_vpc" "accepter" {
-  acc_cidr_block = "172.31.0.0/16"
+  cidr_block = "172.31.0.0/16"
   enable_dns_hostnames = true
 }
 
