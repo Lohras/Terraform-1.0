218156efc70e74d634e8598f9274ad0a5171d897
Lohith raje urs <101534920+Lohras@users.noreply.github.com>
2022-10-17 12:44:59 +0530

    Update variable.tf

--- vpc-peering/variable.tf
+++ vpc-peering/variable.tf
@@ -1,21 +1,21 @@
-variable "req_cidr" {
+variable "vpc_req_cidr" {
     description = "requester CIRD block"
     type = string
-    default = "10.0.0.0/16"  
+#     default = "10.0.0.0/16"  
 }
-variable "acc_cidr" {
+variable "vpc_acc_cidr" {
     description = "accepterter CIRD block"
     type = string
-    default = "172.31.0.0/16"  
+    # default = "172.31.0.0/16"  
 }
 
 variable "req_subnet_cidr" {
     description = "requester CIDR block"
     type = string
-    default = "10.0.1.0/24"  
+    # default = "10.0.1.0/24"  
 }
 variable "acc_subnet_cidr" {
     description = "accepterter CIDR block"
     type = string
-    default = "172.31.1.0/24"  
+    # default = "172.31.1.0/24"  
 }
