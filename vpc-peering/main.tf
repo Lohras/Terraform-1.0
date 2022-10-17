9e96b6b6eb7f1442000ea1389c99192f2820c886
Lohith raje urs <101534920+Lohras@users.noreply.github.com>
2022-10-17 12:34:47 +0530

    Update main.tf

--- vpc-peering/main.tf
+++ vpc-peering/main.tf
@@ -39,26 +39,26 @@ resource "aws_subnet" "accepter" {
     Name = "accepter"
   }
 }
-resource "aws_route_table" "requester" {
-  vpc_id = aws_vpc.requester.id
-  route {
-    cidr_block = var.req_subnet_cidr
-    vpc_peering_connection_id = aws_vpc_peering_connection.my_peer.id
-  }
-}
-resource "aws_route_table" "accepter" {
-  vpc_id = aws_vpc.accepter.id
-  route {
-    cidr_block = var.acc_subnet_cidr
-    vpc_peering_connection_id = aws_vpc_peering_connection.my_peer.id
-  }  
-}
+# resource "aws_route_table" "requester" {
+#   vpc_id = aws_vpc.requester.id
+#   route {
+#     cidr_block = var.req_subnet_cidr
+#     vpc_peering_connection_id = aws_vpc_peering_connection.my_peer.id
+#   }
+# }
+# resource "aws_route_table" "accepter" {
+#   vpc_id = aws_vpc.accepter.id
+#   route {
+#     cidr_block = var.acc_subnet_cidr
+#     vpc_peering_connection_id = aws_vpc_peering_connection.my_peer.id
+#   }  
+# }
 
 
 
 # creating routes from req-vpc2 to acc-default
 resource "aws_route" "requester_main" {
-  route_table_id = aws_route_table.requester.id
+  route_table_id = aws_vpc.requester.main_route_table_id
   destination_cidr_block = var.acc_subnet_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.my_peer.id 
 }
@@ -66,7 +66,7 @@ resource "aws_route" "requester_main" {
 # #Create routes from acc-default to req-vpc2
 
 resource "aws_route" "accepter" {
-  route_table_id = aws_route_table.accepter.id
+  route_table_id = aws_vpc.accepter.main_route_table_id
   destination_cidr_block = var.req_subnet_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.my_peer.id
   
