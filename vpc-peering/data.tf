7e7fd835d92ef04b2883e31a456d1107658e3652
Lohith raje urs <101534920+Lohras@users.noreply.github.com>
2022-10-17 11:57:54 +0530

    Update data.tf

--- vpc-peering/data.tf
+++ vpc-peering/data.tf
@@ -1,24 +1,9 @@
-data "aws_vpc" "requester" {
-  id = var.vpc_id
-}
-data "aws_vpc" "accepter" {
-  id = var.peer_vpc_id
-}
-
-data "aws_subnet" "requester" {
-  id = var.req_subnet_id
-}
-
-data "aws_subnet" "accepter" {
-  id = var.acc_subnet_id
-}
-
 data "aws_route_table" "requester" {
   subnet_id = aws_subnet.requester.id
   # vpc_id = aws_vpc.requester.id
   filter {
     name = "association.main"
-    values = data.aws_route_table.requester.id
+    values = true
   }
 }
 
@@ -26,6 +11,6 @@ data "aws_route_table" "accepter" {
   subnet_id = aws_subnet.accepter.id
   filter {
     name = "association.main"
-    values = data.aws_route_table.accepter.id
+    values = true
   }
 }
