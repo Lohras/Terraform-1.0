8adebad54c95497da8467050bd753aa570409d6e
Lohith raje urs <101534920+Lohras@users.noreply.github.com>
2022-10-18 14:25:43 +0530

    Update main.tf

--- vpc-peering/modules-vpc-complete/main.tf
+++ vpc-peering/modules-vpc-complete/main.tf
@@ -1,34 +1,6 @@
-module "requester_vpc" {
-    source = "../vpc-peering"
-    cidr_block = var.requester_vpc_cidr
-}
-
-module "requester_subnet" { 
-    source = "../vpc-peering"
-    vpc_id = module.requester_vpc.vpc_id
-    cidr_block = module.requester_vpc.vpc_cidr_block
-    nat_gateway_enabled = false
-    nat_instance_enabled = false
-}
-
-module "accepter_vpc" {
-    source = "../vpc-peering"
-    cidr_block = var.accepter_vpc_cidr
-}
-
-module "accepter_subnet" { 
-    source = "../vpc-peering"
-    vpc_id = module.accepter_vpc.vpc_id
-    cidr_block = module.accepter_vpc.vpc_cidr_block
-    nat_gateway_enabled = false
-    nat_instance_enabled = false
-}
-
-module "vpc_peering" {
-    source = "../vpc-peering"
-    auto_accept = true
-    requester_allow_remote_vpc_dns_resolution = true
-    accepter_allow_remote_vpc_dns_resolution  = true
-    requester_vpc_id                          = module.requester_vpc.vpc_id
-    accepter_vpc_id                           = module.accepter_vpc.vpc_id
+module "vpc-peering-single-region" {
+  vpc_req_cidr = "172.35.0.0/16"
+  vpc_acc_cidr = "20.0.0.0/16"
+  req_subnet_cidr = "172.35.1.0/24"
+  acc_subnet_cidr = "20.0.1.0/24"
 }
