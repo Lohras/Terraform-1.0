3ee5500639e20f385ea3974b800280a2e0b50b02
Lohith raje urs <101534920+Lohras@users.noreply.github.com>
2022-10-19 09:04:50 +0530

    Update outputs.tf

--- vpc-peering/modules-vpc-complete/outputs.tf
+++ vpc-peering/modules-vpc-complete/outputs.tf
@@ -1,23 +1,23 @@
 output "requester_vpc" {
-    value = module.vpc-peering-single-region.aws_vpc.requester.id
+    value = module.aws_vpc.requester.id
 }
 output "accepter_vpc" {
-    value = module.vpc-peering-single-region.aws_vpc.accepter.id
+    value = module.aws_vpc.accepter.id
 }
 
 output "connection_id" {
-  value       = module.vpc-peering-single-region.aws_vpc_peering_connection.my_peer.id
+  value       = module.aws_vpc_peering_connection.my_peer.id
   description = "VPC peering connection ID"
 }
 output "requester_subnet" {
-  value = module.vpc-peering-single-region.aws_subnet.requester.id  
+  value = module.aws_subnet.requester.id  
 }
 output "accepter_subnet" {
-  value = module.vpc-peering-single-region.aws_subnet.accepter.id  
+  value = module.aws_subnet.accepter.id  
 }
 output "accepter_route" {
-  value = module.vpc-peering-single-region.aws_route.accepter.id
+  value = module.aws_route.accepter.id
 }
 output "requester_route" {
-  value = module.vpc-peering-single-region.aws_route.requester.id
+  value = module.aws_route.requester.id
 }
