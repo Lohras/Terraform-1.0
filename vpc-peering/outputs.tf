7e814df08900a7388d3cfff2b72d2e7f852a5432
Lohith raje urs <101534920+Lohras@users.noreply.github.com>
2022-10-19 09:07:48 +0530

    Update outputs.tf

--- vpc-peering/outputs.tf
+++ vpc-peering/outputs.tf
@@ -1,20 +1,23 @@
+output "requester_vpc" {
+    value = module.aws_vpc.requester.id
+}
+output "accepter_vpc" {
+    value = module.aws_vpc.accepter.id
+}
+
 output "connection_id" {
-  value = aws_vpc_peering_connection.my_peer.id
+  value       = module.aws_vpc_peering_connection.my_peer.id
+  description = "VPC peering connection ID"
 }
-
-output "requester_id" {
-  value = var.aws_vpc.requester.id
-}
-
-output "accepter_id" {
-  value = var.aws_vpc.accepter.id
-}
-
 output "requester_subnet" {
-  value = var.aws_subnet.requester.id
+  value = module.aws_subnet.requester.id  
 }
-
 output "accepter_subnet" {
-  value = var.aws_subnet.accepter.id
+  value = module.aws_subnet.accepter.id  
 }
-
+output "accepter_route" {
+  value = module.aws_route.accepter.id
+}
+output "requester_route" {
+  value = module.aws_route.requester.id
+}
