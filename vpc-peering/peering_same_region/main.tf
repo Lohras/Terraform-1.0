f9d8611c7c8326fdcf8edca6b2f169c41c4db0da
lrurs <lohith.rajeursks@amadeus.com>
2022-11-21 20:26:47 +0000

    modified

--- vpc-peering/peering_same_region/main.tf
+++ vpc-peering/peering_same_region/main.tf
@@ -1,5 +1,5 @@
 module "vpc-peering-single-region" {
   source = "../../modules/vpc_peering/"
-  req_subnet_cidr = var.req_cidr
-  acc_subnet_cidr = var.acc_cidr
+  req_cidr = var.req_cidr
+  acc_cidr = var.acc_cidr
 }
