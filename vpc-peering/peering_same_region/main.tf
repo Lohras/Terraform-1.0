f9a339982b4fab36ef420dc93738d61b8529a019
lrurs <lohith.rajeursks@amadeus.com>
2022-12-13 23:04:02 +0000

    something again

--- vpc-peering/peering_same_region/main.tf
+++ vpc-peering/peering_same_region/main.tf
@@ -6,17 +6,25 @@ module "vpc-peering-single-region" {
   accepter_vpc_id = var.accepter_vpc_id
 }
 
-# creating routes from req-vpc2 to acc-default
-resource "aws_route" "requester" {
-  route_table_id = data.aws_route_tables.requestor.ids[0]
-  destination_cidr_block = var.acc_cidr
-  vpc_peering_connection_id = module.vpc-peering-single-region.peering_id
+# # creating routes from req-vpc2 to acc-default
+# resource "aws_route" "requester" {
+#   route_table_id = data.aws_route_tables.requestor.ids[0]
+#   destination_cidr_block = var.acc_cidr
+#   vpc_peering_connection_id = module.vpc-peering-single-region.peering_id
+# }
+
+# # #Create routes from acc-default to req-vpc2
+
+# resource "aws_route" "accepter" {
+#   route_table_id = data.aws_route_tables.acceptor.ids[0]  
+#   destination_cidr_block = var.req_cidr
+#   vpc_peering_connection_id = module.vpc-peering-single-region.peering_id
+# }
+
+output "requester" {
+  value = data.aws_route_tables.requestor.ids[0]
 }
 
-# #Create routes from acc-default to req-vpc2
-
-resource "aws_route" "accepter" {
-  route_table_id = data.aws_route_tables.acceptor.ids[0]  
-  destination_cidr_block = var.req_cidr
-  vpc_peering_connection_id = module.vpc-peering-single-region.peering_id
+output "accepter" {
+  value = data.aws_route_tables.acceptor.ids[0]
 }
